9d0140474746e8996d7c5f5cd73fe1ee5a4a223e
6110110628 <60842378+6110110628@users.noreply.github.com>
2020-02-13 00:38:39 +0700

    Add files via upload

--- Pokemon.java
+++ Pokemon.java
@@ -1,7 +1,56 @@
-/**
- * Pokemon
- */
-public class Pokemon {
+import java.util.Scanner;
 
+
+class Pokemon{
+    Scanner sc = new Scanner(System.in);
     
+    private String option;
+    private String name; //name character
+    private String nPokemon;
+    private int type; 
+
+    public Pokemon(){
+        this.type =(int)(((double)Math.random())*4);
+    }
+    
+    public void pageStart() {
+        System.out.print("\n\n\t\t Enter Your name : ");
+        this.name = sc.nextLine();
+        System.out.print("\n\n\t\t Enter Your Buddy Pokemon name : ");
+        this.nPokemon = sc.nextLine();
+        System.out.print("\n\n\n\n\n");
+        
+        
+
+    }
+    
+    public void pageStatus(){
+        System.out.print("\n\t   ==========================================================\n"
+                           +"\n\t\t  Name = "+this.name
+                           +"\n\t\t  My buddy pokemon = "+this.nPokemon
+                           +"\n\t\t  type = "+this.type
+                           +"\n\n\t  ============================================================\n");
+   }
+
+   public void pageIndex(){
+       System.out.print("\n\t ===================================================="
+                           +"\n\t\t ===== WELLCOM TO POKEMON GAME =====");
+   }
+
+   public String pageOption(){
+       System.out.print("\n\n\t ----------------------------------------------------------"
+                       +"\n\t\t 1.Start"
+                       +"\n\t\t 2.Quit");
+       System.out.print("\n\n -----------------------------------------------------------\n\n\n\n\n");
+       this.option = sc.nextLine();
+       System.out.print("\n\n ############################################################\n\n\n\n\n");
+       return this.option;
+
+   }
+
+   public void pageQuit(){
+       System.out.print("\n\t\t------------------------------------End Game------------------------------------");
+   }
+
+
 }
--- PokemonGame.java
+++ PokemonGame.java
@@ -1,9 +1,22 @@
-/**
- * PokemonGame
- */
-public class PokemonGame {
+import java.util.Scanner;
 
-    public static void main(String[] args){
+public class PokemonGame{
+
+    public static void main(String [] args){           
+        String options;
+        Pokemon p = new Pokemon();
+        p.pageIndex();
         
+        options = p.pageOption();
+
+        if(options.equals("1")){
+            p.pageStart();
+            p.pageStatus();  
+ 
+         }
+         else if(options.equals("2")){
+            p.pageQuit();
+
+         }
     }
 }
