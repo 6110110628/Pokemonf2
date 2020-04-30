99e781269a52a7655dc7320bf9f54b7fd2bb375e
6110110628 <60842378+6110110628@users.noreply.github.com>
2020-04-30 22:40:19 +0700

    Update PokemonGame.java

--- PokemonGame.java
+++ PokemonGame.java
@@ -4,8 +4,8 @@ public class PokemonGame {
     public static void main(String[] args){
         System.out.println("---Wellcome to Pokemon Game---");
         Trainer t = new Trainer("Alee");
-        MainGameGUI mg = new MainGameGUI(t);
-        //t.play();
+        //MainGameGUI mg = new MainGameGUI(t);
+        t.play();
         System.out.println("\n\n---See you---");
     }
 }
